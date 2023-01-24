c8fc66cd8d0269513398bf798b2146f8b3512d75
David Jandron <jandron1211@gmail.com>
2023-01-24 04:06:36 -0800

    ADD user interface logic and html content and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,6 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 };
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
-}
-
 AddressBook.prototype.findContact = function(id) {
   if (this.contacts[id] !== undefined) {
     return this.contacts[id];
@@ -36,6 +29,31 @@ AddressBook.prototype.deleteContact = function(id) {
   return true;
 };
 
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 };
+
+//User Interface Logic
+
+let addressBook = new AddressBook();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  addressBook.addContact(newContact);
+  console.log(addressBook.contacts);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
