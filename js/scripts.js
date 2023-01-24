d597a03cf80f5a6b12d7d7a5ca0635dc4451ce2d
David Jandron <jandron1211@gmail.com>
2023-01-24 04:58:14 -0800

    ADD function to display contacts in DOM

--- js/scripts.js
+++ js/scripts.js
@@ -41,9 +41,28 @@ Contact.prototype.fullName = function() {
 };
 
 //User Interface Logic
-
+//variable is global because declared at top level of our file. generally avoid, but here it mimics a database
 let addressBook = new AddressBook();
 
+//function that diplays contacts in DOM. Takes address book object as an argument
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
+//define function handle form submission in window load event listener
+//we call prevent default to prevent page refresh, gather user uput from form, create new contact object
+//add new contact by using address book .prototype .addContact() method
+//log list of contacts to console to doible check new contact has been added
 function handleFormSubmission(event) {
   event.preventDefault();
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
@@ -54,6 +73,7 @@ function handleFormSubmission(event) {
   console.log(addressBook.contacts);
 }
 
+//event listener for load event. called when page has loaded all resources. 
 window.addEventListener("load", function (){
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
 });
